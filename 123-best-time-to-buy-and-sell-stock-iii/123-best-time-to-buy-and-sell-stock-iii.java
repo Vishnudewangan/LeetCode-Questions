d02fe67531769b45f31c9345291018974b72c28f
Vishnudewangan <82579731+Vishnudewangan@users.noreply.github.com>
2022-09-13 17:28:00 +0530

    Time: 8 ms (62.51%), Space: 80.9 MB (49.99%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,56 +1,44 @@
 class Solution {
     public int maxProfit(int[] prices) {
+          int n=prices.length;
         
-        int n= prices.length;
-        int[] dp1=new int[n];// dp1[i] =-> 0 to i max profit with one transaction
-        int[] dp2=new int[n]; // dp2[i] =-> i to n max profit with one transaction
+        int[]dp1 = new int[n]; // dp1[i] -> 0 to i one transaction best result
+        int[]dp2 = new int[n]; // dp2[i] -> i to n one transaction best result
         
+        //fill dp1
         int lpsf = prices[0];
-        
-        for(int i=1;i<n;i++)
-        {
-            if(prices[i] < lpsf)
-            {
-              lpsf= prices[i];
+        for(int i=1;i < n;i++) {
+            if(prices[i] < lpsf) {
+                lpsf = prices[i];
             }
             
-            int cbp = dp1[i-1]; // completed before profit
-            int cip = prices[i] - lpsf ; // completed at i profit
+            int cb = dp1[i-1]; // completed before i
+            int ci = prices[i] - lpsf; // completed at i
             
-            
-            dp1[i] = Math.max(cbp , cip);
+            dp1[i] = Math.max(cb,ci);
         }
         
+        //fill dp2
         int mpsf = prices[n-1];
-        
-        
-        for(int i=n-2;i>=0;i--)
-        {
-            if(prices[i] > mpsf)
-            {
+        for(int i=n-2;i >= 0;i--) {
+            if(prices[i] > mpsf) {
                 mpsf = prices[i];
             }
             
-             int cbp = dp2[i+1]; // completed after profit
-            int cip = mpsf-prices[i] ; // buy point at i 
+            int ca = dp2[i+1]; //completed after i
+            int ci = mpsf - prices[i];// buy point is i
             
-            
-            dp2[i] = Math.max(cbp , cip);
+            dp2[i] = Math.max(ca,ci);
         }
         
+        int max = 0;
         
-        int max= 0;
-        
-        for(int i=0;i<n;i++)
-        {
-            if(dp1[i]+ dp2[i] >max)
-            {
-                max= dp1[i] + dp2[i];
+        for(int i=0; i < n;i++) {
+            if(dp1[i] + dp2[i] > max) {
+                max = dp1[i] + dp2[i];
             }
         }
         
         return max;
-        
-        
     }
 }
