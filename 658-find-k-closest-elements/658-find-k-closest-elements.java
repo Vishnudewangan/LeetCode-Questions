dfbd426fe578b3d960113d47ae3874dcf45dd951
Vishnudewangan <82579731+Vishnudewangan@users.noreply.github.com>
2022-09-29 12:47:16 +0530

    Time: 9 ms (61.13%), Space: 62.2 MB (66.29%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.java
+++ 658-find-k-closest-elements/658-find-k-closest-elements.java
@@ -1,27 +1,45 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
+       
         
+//         // O(n);
         
-        int left = 0 , right = arr.length-1;
+//         int left = 0 , right = arr.length-1;
         
-        while(right - left >=k)
-        {
-            if(x-arr[left] <= arr[right]-x)
-            {
-                right--;
-            }
-            else
-            {
-                left++;
-            }
+//         while(right - left >=k)
+//         {
+//             if(x-arr[left] <= arr[right]-x)
+//             {
+//                 right--;
+//             }
+//             else
+//             {
+//                 left++;
+//             }
             
-        }
-         List<Integer> ans =new ArrayList<>();
-        for(int i=left; i<=right;i++)
-        {
-            ans.add(arr[i]);
-        }
+//         }
+//          List<Integer> ans =new ArrayList<>();
+//         for(int i=left; i<=right;i++)
+//         {
+//             ans.add(arr[i]);
+//         }
         
-        return ans;
+//         return ans;
+        
+        
+        //  O(log(n-k))
+        
+        
+      
+        int left = 0, right = arr.length - k;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (x - arr[mid] > arr[mid + k] - x)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return Arrays.stream(arr, left, left + k).boxed().collect(Collectors.toList());
+    
     }
 }
