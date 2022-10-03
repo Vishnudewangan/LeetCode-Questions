1683e7d605128523c72b28ec01a4a530e835ed9e
Vishnudewangan <82579731+Vishnudewangan@users.noreply.github.com>
2022-10-04 01:41:38 +0530

    Time: 1 ms (91.44%), Space: 41.6 MB (81.52%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.java
+++ 14-longest-common-prefix/14-longest-common-prefix.java
@@ -1,62 +1,12 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if(strs.length==1) return strs[0];
-        
-       
-         
-        String s1=strs[0];
-        String s2=strs[1];
-        
-        
-        int k=0,l=0;
-       int count=0;
-        
-        while(k<s1.length() && l<s2.length())
-        {
-            if(s1.charAt(k)==s2.charAt(l))
-            {
-                count++;
-            }
-            else
-            {
-                break;
-            }
-            k++;
-            l++;
-        }
-        
-        if(count==0) return "";
-        
-        String match = s1.substring(0,count);
-        System.out.println(match);
-        
-        for(int i=2;i<strs.length;i++)
-        {
-             String str =strs[i];
-            int len = str.length();
-            count=0;
-            
-            for(int j=0;j<(match.length()<len? match.length():len);j++)
-            {
-                if(str.charAt(j)==match.charAt(j))
-                {
-                    count++;
-                    System.out.println(count);
-                }
-                else
-                {
-                    break;
-                }
-            }
-                
-                if(count==0)
-                {
-                    return "";
-                }
-                match=str.substring(0,count);
-        }
-                
-          return match;      
-        
+         if (strs.length == 0) return "";
+    String prefix = strs[0];
+    for (int i = 1; i < strs.length; i++)
+        while (strs[i].indexOf(prefix) != 0) {
+            prefix = prefix.substring(0, prefix.length() - 1);
+            if (prefix.isEmpty()) return "";
+        }        
+    return prefix;
     }
 }
