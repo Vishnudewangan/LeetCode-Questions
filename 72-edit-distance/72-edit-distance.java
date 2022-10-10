bb96c2ad521ff2b91c885b75707986f824d649a8
Vishnudewangan <82579731+Vishnudewangan@users.noreply.github.com>
2022-10-10 16:39:42 +0530

    Time: 7 ms (81.46%), Space: 44.1 MB (82.36%) - LeetHub

--- 72-edit-distance/72-edit-distance.java
+++ 72-edit-distance/72-edit-distance.java
@@ -1,27 +1,33 @@
 public class Solution {
     public int minDistance(String word1, String word2) {
-        int m = word1.length();
-        int n = word2.length();
         
-        int[][] cost = new int[m + 1][n + 1];
-        for(int i = 0; i <= m; i++)
-            cost[i][0] = i;
-        for(int i = 1; i <= n; i++)
-            cost[0][i] = i;
+        int[][] dp=new int[word1.length()+1][word2.length()+1];
         
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(word1.charAt(i) == word2.charAt(j))
-                    cost[i + 1][j + 1] = cost[i][j];
-                else {
-                    int a = cost[i][j];
-                    int b = cost[i][j + 1];
-                    int c = cost[i + 1][j];
-                    cost[i + 1][j + 1] = a < b ? (a < c ? a : c) : (b < c ? b : c);
-                    cost[i + 1][j + 1]++;
-                }
-            }
+        for(int[]arr : dp)
+        {
+            Arrays.fill(arr,-1);
         }
-        return cost[m][n];
+        return helper(word1,word2,word1.length(),word2.length(),dp);
+    }
+    int helper(String word1, String word2,int idx1,int idx2,int[][] dp)
+    {
+        if(idx1==0) return idx2;
+        
+        if(idx2==0) return idx1;
+        
+        if(dp[idx1][idx2]!=-1) return dp[idx1][idx2];
+        
+        if(word1.charAt(idx1-1) == word2.charAt(idx2-1))
+        {
+            return dp[idx1][idx2]=helper(word1,word2,idx1-1,idx2-1,dp);
+        }
+        else
+        {
+            int a = helper(word1,word2,idx1,idx2-1,dp); // inserting 
+            int b = helper(word1,word2,idx1-1,idx2,dp); // deleting
+            int c = helper(word1,word2,idx1-1,idx2-1,dp); // Replace
+            
+            return dp[idx1][idx2]=1 + Math.min(a,Math.min(b,c));
+        }
     }
 }
