02079f9a424078370809fe63396106a676791f3e
Vishnudewangan <82579731+Vishnudewangan@users.noreply.github.com>
2022-10-30 12:58:23 +0530

    Time: 802 ms (78.47%), Space: 0B (100.00%) - LeetHub

--- 1407-top-travellers/1407-top-travellers.sql
+++ 1407-top-travellers/1407-top-travellers.sql
@@ -3,3 +3,4 @@
 Select Distinct u.name, IFNULL(sum(distance) Over (Partition by u.id) , 0) as travelled_distance
 from Users u Left Join Rides r on u.id = r.user_id
 Order by travelled_distance desc, u.name asc;
+
