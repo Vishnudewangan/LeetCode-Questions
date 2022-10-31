6a59a4035f4bd3d71874e3ce352f834d7d0ec12c
Vishnudewangan <82579731+Vishnudewangan@users.noreply.github.com>
2022-11-01 00:02:51 +0530

    Time: 2 ms (52.87%), Space: 43.1 MB (11.44%) - LeetHub

--- 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.java
+++ 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.java
@@ -1,18 +1,36 @@
 class Solution {
     public int maximumDifference(int[] nums) {
         
-        int ans =-1;
-        int n = nums.length;
         
-        for(int i=0;i<n;i++)
+        /// *** O(n^2) ***
+//         int ans =-1;
+//         int n = nums.length;
+        
+//         for(int i=0;i<n;i++)
+//         {
+//             for(int j=i+1;j<n;j++)
+//             {    
+//                 if(nums[i] < nums[j])
+//                 ans = Math.max(ans, nums[j] - nums[i]);
+//             }
+//         }
+        
+//         return ans;
+        
+        // ***** O(n) *** Best Time to buy ans sell stock if no profit happens then had to return 0 now we have to return -1;
+        
+        int n= nums.length;
+        int res = -1;
+        
+        int min= nums[0];
+        
+        for(int i=1;i<n;i++)
         {
-            for(int j=i+1;j<n;j++)
-            {    
-                if(nums[i] < nums[j])
-                ans = Math.max(ans, nums[j] - nums[i]);
-            }
+            res = Math.max(res, nums[i] - min);
+            
+            min = Math.min(min,nums[i]);
         }
         
-        return ans;
+        return res==0? -1 : res;
     }
 }
