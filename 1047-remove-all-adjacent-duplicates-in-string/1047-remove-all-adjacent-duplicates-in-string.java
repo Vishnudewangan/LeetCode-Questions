903239e7a983eae073b0cb6677145398a43e8108
Vishnudewangan <82579731+Vishnudewangan@users.noreply.github.com>
2022-11-10 12:25:29 +0530

    Time: 148 ms (33.19%), Space: 55 MB (51.98%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -0,0 +1,65 @@
+class Solution {
+    public String removeDuplicates(String s) {
+        
+        Stack<Pair> st =new Stack<>();
+        
+        for(char ch : s.toCharArray())
+        {  
+             
+            if(st.size()==0)
+            {
+                st.push(new Pair(ch,1));
+            }
+            else if(st.peek().c==ch && st.peek().cnt<2)
+            {
+               st.peek().cnt=st.peek().cnt +1;
+            }
+            else
+            {
+                if(st.peek().cnt==2) st.pop();
+                
+                if(st.size()>0 && st.peek().c==ch)
+                {
+                    st.peek().cnt = st.peek().cnt+1;
+                }
+                else
+                {
+                    st.push(new Pair(ch,1));
+                }
+               
+                
+            }
+        }
+        
+        if(st.peek().cnt ==2) st.pop();
+        
+        StringBuilder sb =new StringBuilder();
+        
+        
+        while(st.size()>0)
+        {
+           Pair rem = st.pop();
+            
+            for(int i=1;i<=rem.cnt ;i++)
+            {
+                sb.append(rem.c);
+            }
+        }
+        
+        return sb.reverse().toString();
+    }
+    
+    static class Pair
+    {
+        char c;
+        int cnt;
+        
+        Pair(char c, int cnt)
+        {
+            this.c=c;
+            this.cnt = cnt;
+        }
+    }
+    
+    
+}
