b17852c0f12f24bf77f9bbfc1b54ff19847b8fe1
Vishnudewangan <82579731+Vishnudewangan@users.noreply.github.com>
2022-11-10 16:02:30 +0530

    Time: 13 ms (78.64%), Space: 54.5 MB (34.30%) - LeetHub

--- 0934-shortest-bridge/0934-shortest-bridge.java
+++ 0934-shortest-bridge/0934-shortest-bridge.java
@@ -1,80 +1,75 @@
 class Solution {
-    
-    
-  
     public int shortestBridge(int[][] grid) {
-            int[][] dir = {{-1,0},{0,1},{1,0},{0,-1}};
-           Queue<int[]> que=new LinkedList<>();
-      outer:  for(int i=0;i<grid.length;i++)
+        
+        int[][] dir ={{-1,0},{0,1},{1,0},{0,-1}};
+        
+        Queue<int[]> que =new ArrayDeque<>();
+        
+       outer: for(int i=0;i<grid.length;i++)
         {
             for(int j=0;j<grid[0].length;j++)
-            {   
-                if(grid[i][j]==1){
-                getConnectedComponent(i,j,grid,que);
-                break outer ;
+            {
+                if(grid[i][j]==1)
+                {
+                    getConnectedComponent(i,j,grid,que);
+                    break outer;
                 }
             }
         }
         
-       // System.out.println(que);
         
-        int step=0;
-         
+        int steps =0;
+        
         while(que.size()>0)
         {
+            int size=que.size();
             
-            int size= que.size();
-            for(int c=0;c<size;c++)
+            for(int i=0;i<size;i++)
             {
-            int[] pos= que.remove();
-            
-            int x=pos[0];
-            int y=pos[1];
-            
-            for(int i=0;i<4;i++)
-            {
-                int xx=dir[i][0]+x;
-                int yy = dir[i][1]+y;
+               int[] rem = que.remove();
+                int x= rem[0];
+                int y = rem[1];
                 
-                if(xx>=0 && yy>= 0 && xx<grid.length && yy<grid[0].length && grid[xx][yy]!=2)
+                for(int d=0;d<4;d++)
                 {
-                   
-                
-                if(grid[xx][yy]== 1) return step;
-                    grid[xx][yy]=2;
-                que.add(new int[]{xx, yy});
-               
+                    int xx= x + dir[d][0];
+                    int yy = y+ dir[d][1];
+                    
+                    if(xx>=0 && yy>=0 && xx<grid.length && yy< grid[0].length && grid[xx][yy]!=2)
+                    {    
+                        if(grid[xx][yy]==1) return steps;
+                        que.add(new int[]{xx,yy});
+                        
+                        grid[xx][yy]=2;
+                        
+                    }
                 }
-                
-                
             }
-                
-            }
-            step++;
+            
+            steps++;
+            
             
         }
         
-        return -1;
-        
+        return steps;
     }
     
-    void  getConnectedComponent(int i, int j, int[][] grid, Queue<int[]> que)
-    {
-        if(i<0 || j<0 || i>=grid.length || j>= grid[0].length || grid[i][j]!=1)
+    void getConnectedComponent(int i,int j,int[][] grid,Queue<int[]> que){
+        
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]!=1)
         {
-            return ;
+            return;
         }
         
+        grid[i][j] = 2;
+        
         que.add(new int[]{i,j});
-      
-        grid[i][j]=2;
         
-       getConnectedComponent(i-1,j,grid,que);
-        
+        getConnectedComponent(i-1,j,grid,que);
         getConnectedComponent(i,j+1,grid,que);
         getConnectedComponent(i+1,j,grid,que);
-        getConnectedComponent(i,j-1,grid,que);
-        
-        
+        getConnectedComponent(i,j-1,grid,que);     
     }
+    
+    
 }
