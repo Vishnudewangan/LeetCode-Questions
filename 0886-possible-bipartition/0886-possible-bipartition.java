6bf21e82867f06ca53c4c89ca2358cda6d77b94f
Vishnudewangan <82579731+Vishnudewangan@users.noreply.github.com>
2022-11-19 00:23:18 +0530

    Time: 37 ms (65.61%), Space: 72.9 MB (55.50%) - LeetHub

---
+++ 0886-possible-bipartition/0886-possible-bipartition.java
@@ -0,0 +1,46 @@
+class Solution {
+    public boolean possibleBipartition(int n, int[][] dislikes) {
+	List<Integer>[] graph = new ArrayList[n];
+	for (int i = 0; i < n; i++) {
+	  graph[i] = new ArrayList<>();
+	}
+
+	for (int[] dislike : dislikes) {
+	  int u = dislike[0] - 1;
+	  int v = dislike[1] - 1;
+
+	  graph[u].add(v);
+	  graph[v].add(u);
+	}
+
+	int[] colors = new int[n];
+
+	for (int i = 0; i < n; i++) {
+	  if (colors[i] != 0) {
+		continue;
+	  }
+
+	  colors[i] = 1;
+
+	  Queue<Integer> queue = new LinkedList<>();
+	  queue.add(i);
+
+	  while (!queue.isEmpty()) {
+		int node = queue.poll();
+
+		for (int adj : graph[node]) {
+		  if (colors[adj] == colors[node]) {
+			return false;
+		  }
+
+		  if (colors[adj] == 0) {
+			colors[adj] = -colors[node];
+			queue.add(adj);
+		  }
+		}
+	  }
+	}
+
+	return true;
+  }
+}
