a07ff0766559d0a4f4699088895fdf8e2ee7ff96
Vishnudewangan <82579731+Vishnudewangan@users.noreply.github.com>
2022-12-04 20:06:46 +0530

    Time: 1 ms (68.47%), Space: 45.9 MB (16.56%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -16,39 +16,87 @@ class Node {
 class Solution {
     public Node copyRandomList(Node head) {
         
-          Node th = new Node(-1);
-          Node dt = th;
+         // HashMap method
         
-        HashMap<Node,Node> map =new HashMap<>(); /// old node vs new Node
+//           Node th = new Node(-1);
+//           Node dt = th;
         
+//         HashMap<Node,Node> map =new HashMap<>(); /// old node vs new Node
+        
+        
+//         Node curr= head;
+        
+//         while(curr!=null)
+//         {
+//             Node node = new Node(curr.val);
+            
+//             dt.next = node;
+            
+//             map.put(curr,node);
+            
+//             dt = dt.next;
+//             curr = curr.next;
+//         }
+        
+//          curr= head;
+        
+//         Node ans= th.next;
+        
+//         while(curr!=null)
+//         {
+//             ans.random = curr.random==null ? null : map.get(curr.random);
+//             curr = curr.next;
+//             ans = ans.next;
+//         }
+        
+//         return th.next;
+        
+        // O(1)
+        
+        if(head==null) return null;
         
         Node curr= head;
         
         while(curr!=null)
         {
-            Node node = new Node(curr.val);
+            Node currNext = curr.next;
             
-            dt.next = node;
+            Node nn= new Node(curr.val);
             
-            map.put(curr,node);
+            curr.next= nn;
+            nn.next = currNext;
             
-            dt = dt.next;
-            curr = curr.next;
+            curr = currNext;
         }
         
-         curr= head;
         
-        Node ans= th.next;
+        curr = head;
         
         while(curr!=null)
         {
-            ans.random = curr.random==null ? null : map.get(curr.random);
-            curr = curr.next;
-            ans = ans.next;
+            curr.next.random = curr.random==null ? null : curr.random.next;
+            
+            curr= curr.next.next;
+        }        
+        
+        
+        Node c1=head;
+        Node ans= c1.next;
+        Node c2 = c1.next;
+        
+        while(c1!=null && c2!=null)
+        {
+            Node n1= c2.next;
+            Node n2 = n1!=null ? n1.next: null;
+            
+            c1.next = n1 ;
+            c2.next= n2;
+            
+            c1=n1;
+            c2=n2;
         }
         
-        return th.next;
-        
+        return ans;
         
     }
 }
