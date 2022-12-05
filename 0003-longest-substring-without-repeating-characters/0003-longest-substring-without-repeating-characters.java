5b714c5e2753bc7c11e4501a6a4e718403246471
Vishnudewangan <82579731+Vishnudewangan@users.noreply.github.com>
2022-12-05 20:30:29 +0530

    Time: 8 ms (80.21%), Space: 42.5 MB (87.64%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,46 +1,49 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         
-        int n = s.length();
         int i=-1;
         int j=-1;
+        
+        HashMap<Character,Integer> map =new HashMap<>();
+        
         int maxLen = 0;
         
-        HashMap<Character,Integer> map=new HashMap<>();
-        
-        while(i<n-1)
+        while(i<s.length()-1)
         {
-            while(i<n-1)
+            while(i<s.length()-1)
             {
                 i++;
                 
-                char val = s.charAt(i);
+                char ch = s.charAt(i);
                 
-                map.put(val,map.getOrDefault(val,0)+1);
+               map.put(ch,map.getOrDefault(ch,0)+1);
                 
-                if(map.get(val)==2)
+                if(map.get(ch)==1)
+                {
+                   int len =i-j;
+                    
+                    maxLen = Math.max(len,maxLen);
+                }
+                else if(map.get(ch)==2)
                 {
                     break;
                 }
-                
-                int len = i-j;
-                
-                maxLen = Math.max(len, maxLen);
-                
             }
+            
             while(j<i)
             {
                 j++;
+                char ch = s.charAt(j);
                 
-                char val =s.charAt(j);
+                int nf = map.get(ch);
                 
-                if(map.get(val)==1)
+                if(nf==1)
                 {
-                    map.remove(val);
+                    map.remove(ch);
                 }
-                else if(map.get(val)==2)
+                else if(nf==2)
                 {
-                    map.put(val,1);
+                    map.put(ch,1);
                     break;
                 }
             }
