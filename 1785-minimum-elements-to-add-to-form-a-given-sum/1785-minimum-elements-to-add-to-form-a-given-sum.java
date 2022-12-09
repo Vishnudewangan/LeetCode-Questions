5622cd96f92e4c65b8bfe1a93cb9de973a7f6706
Vishnudewangan <82579731+Vishnudewangan@users.noreply.github.com>
2022-12-09 23:07:49 +0530

    Time: 2 ms (82.29%), Space: 82.4 MB (55.21%) - LeetHub

---
+++ 1785-minimum-elements-to-add-to-form-a-given-sum/1785-minimum-elements-to-add-to-form-a-given-sum.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int minElements(int[] nums, int limit, int goal) {
+       long sum = 0;
+	for (int i = 0; i < nums.length; i++) {
+		sum += nums[i];
+	}
+	long delta = (long)goal - sum;
+	return (int)((Math.abs(delta) + limit - 1) / limit);  // will need the ceil in such a integer division here.
+    }
+}
