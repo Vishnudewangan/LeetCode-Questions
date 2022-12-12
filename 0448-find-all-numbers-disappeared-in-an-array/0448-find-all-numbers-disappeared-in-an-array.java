f2d5f67592ba01254f1a99754e442327c5953794
Vishnudewangan <82579731+Vishnudewangan@users.noreply.github.com>
2022-12-12 22:39:26 +0530

    Time: 10 ms (72.60%), Space: 68.9 MB (25.00%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -1,20 +1,44 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
+        // Time - O(n) , space- O(n)
+//         HashMap<Integer,Integer> map = new HashMap<>();
         
-        HashMap<Integer,Integer> map = new HashMap<>();
-        
-        for(int val : nums)
-        {
-            map.put(val,map.getOrDefault(0,1)+1);
-        }
+//         for(int val : nums)
+//         {
+//             map.put(val,map.getOrDefault(0,1)+1);
+//         }
         
        
         
-        List<Integer> list = new ArrayList<>();
+//         List<Integer> list = new ArrayList<>();
         
-        for(int i=1;i<=nums.length;i++)
+//         for(int i=1;i<=nums.length;i++)
+//         {
+//             if(!map.containsKey(i))list.add(i);
+//         }
+        
+//         return list;
+        
+        // Time - O(n) , Space-O(1)
+        
+        for(int i=0;i<nums.length;i++)
         {
-            if(!map.containsKey(i))list.add(i);
+            int orgVal = Math.abs(nums[i]);
+            int idx = orgVal - 1;
+            
+            if(nums[idx]>0)
+            {
+                nums[idx]*=-1;
+            }
+        }
+        
+        List<Integer> list=new ArrayList<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]>0)
+            {
+                list.add(i+1);
+            }
         }
         
         return list;
