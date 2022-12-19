f3f09215bfea84cbf49b0dde5429f9f59bd54b2c
Vishnudewangan <82579731+Vishnudewangan@users.noreply.github.com>
2022-12-19 12:24:25 +0530

    Time: 436 ms (5.26%), Space: 84.6 MB (24.21%) - LeetHub

---
+++ 0870-advantage-shuffle/0870-advantage-shuffle.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] advantageCount(int[] A, int[] B) {
+        
+       TreeMap<Integer, Integer> m = new TreeMap<>();
+        for (int val : A) m.put(val, m.getOrDefault(val, 0) + 1);
+        int[] res = new int[A.length];
+        for (int i = 0; i < A.length; ++i) {
+            Integer x = m.higherKey(B[i]);
+            if (x == null) x = m.firstKey();
+            m.put(x, m.get(x) - 1);
+            if (m.get(x) == 0) m.remove(x);
+            res[i] = x;
+        }
+        return res;
+    }
+}
