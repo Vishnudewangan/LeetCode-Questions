736c1cc62510409e289afa7ed199731775f3c5ea
Vishnudewangan <82579731+Vishnudewangan@users.noreply.github.com>
2022-12-21 15:30:17 +0530

    Time: 24 ms (85.44%), Space: 72.7 MB (56.32%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.java
+++ 0886-possible-bipartition/0886-possible-bipartition.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean possibleBipartition(int n, int[][] dislikes) {
-	List<Integer>[] graph = new ArrayList[n];
+        List<Integer>[] graph = new ArrayList[n];
 	for (int i = 0; i < n; i++) {
 	  graph[i] = new ArrayList<>();
 	}
@@ -42,5 +42,5 @@ class Solution {
 	}
 
 	return true;
-  }
+    }
 }
