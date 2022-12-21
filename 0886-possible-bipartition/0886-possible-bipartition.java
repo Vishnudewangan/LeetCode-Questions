64a74075d5ccafcc41a3294153087d9017e77f21
Vishnudewangan <82579731+Vishnudewangan@users.noreply.github.com>
2022-12-21 15:40:27 +0530

    Time: 90 ms (25.61%), Space: 70.4 MB (68.25%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.java
+++ 0886-possible-bipartition/0886-possible-bipartition.java
@@ -1,46 +1,77 @@
 class Solution {
+    
+     static class Pair{
+         int v;
+         int sn;
+         
+         Pair(int v, int sn)
+         {
+             this.v= v;
+             this.sn=sn;
+         }
+     }
     public boolean possibleBipartition(int n, int[][] dislikes) {
-        List<Integer>[] graph = new ArrayList[n];
-	for (int i = 0; i < n; i++) {
-	  graph[i] = new ArrayList<>();
-	}
-
-	for (int[] dislike : dislikes) {
-	  int u = dislike[0] - 1;
-	  int v = dislike[1] - 1;
-
-	  graph[u].add(v);
-	  graph[v].add(u);
-	}
-
-	int[] colors = new int[n];
-
-	for (int i = 0; i < n; i++) {
-	  if (colors[i] != 0) {
-		continue;
-	  }
-
-	  colors[i] = 1;
-
-	  Queue<Integer> queue = new LinkedList<>();
-	  queue.add(i);
-
-	  while (!queue.isEmpty()) {
-		int node = queue.poll();
-
-		for (int adj : graph[node]) {
-		  if (colors[adj] == colors[node]) {
-			return false;
-		  }
-
-		  if (colors[adj] == 0) {
-			colors[adj] = -colors[node];
-			queue.add(adj);
-		  }
-		}
-	  }
-	}
-
-	return true;
+        
+       ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        
+        for(int i=0;i<n;i++)
+        {
+            graph.add(new ArrayList<>());
+        }
+        for(int[] dislike : dislikes)
+        {
+            int u = dislike[0]-1;
+            int v= dislike[1]-1;
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+        
+        int[] vis=new int[n];
+        Arrays.fill(vis,-1);
+        
+      for(int i=0;i<n;i++)
+      {
+          if(vis[i]==-1)
+          {
+              boolean comp=compBipartite(i,graph,vis);
+              if(comp==false) return false;
+          }
+      }
+        
+        return true;
+        
+    }
+    boolean compBipartite(int src, ArrayList<ArrayList<Integer>> graph,int[] vis)
+    {
+          Queue<Pair> que =new ArrayDeque<>();
+        
+        que.add(new Pair(src,0));
+        while(que.size()>0)
+        {
+            Pair rem =que.remove();
+            
+            if(vis[rem.v]!=-1)
+            {
+                int osn = vis[rem.v];
+                int nsn = rem.sn;
+                
+                if(osn!=nsn)
+                {
+                    return false;
+                }
+                continue;
+            }
+            vis[rem.v] = rem.sn;
+            
+            for(int nbr : graph.get(rem.v))
+            {
+                int sn = rem.sn==0?1 : 0;
+                
+                que.add(new Pair(nbr,sn));
+            }
+        }
+        
+        return true;
+        
     }
 }
