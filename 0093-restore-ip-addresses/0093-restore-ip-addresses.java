cc094b43829c7a27e2c33e5c1f1ced41d1e0ebe6
Vishnudewangan <82579731+Vishnudewangan@users.noreply.github.com>
2023-01-21 16:39:19 +0530

    Time: 9 ms (37.65%), Space: 42.8 MB (45.21%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.java
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.java
@@ -1,21 +1,44 @@
 class Solution {
     public List<String> restoreIpAddresses(String s) {
-     List<String> solutions = new ArrayList<String>();
-    restoreIp(s, solutions, 0, "", 0);
-    return solutions;
-}
-
-private void restoreIp(String ip, List<String> solutions, int idx, String restored, int count) {
-    if (count > 4) return;
-    if (count == 4 && idx == ip.length()) solutions.add(restored);
-    
-    for (int i=1; i<4; i++) {
-        if (idx+i > ip.length()) break;
-        String s = ip.substring(idx,idx+i);
-        if ((s.startsWith("0") && s.length()>1) || (i==3 && Integer.parseInt(s) >= 256)) continue;
-        restoreIp(ip, solutions, idx+i, restored+s+(count==3?"" : "."), count+1);
+        
+        List<String> res = new ArrayList<>();
+        
+        helper(s,0,0,"",res);
+        return res;
     }
     
+    void helper(String str, int i, int par , String ans, List<String> res)
+    {
+          if(i==str.length() || par== 4)
+          {
+              if(i==str.length() && par==4)
+              {
+                res.add(ans.substring(0,ans.length()-1));  
+              } 
+              
+              return;
+          }
+        
+          helper(str,i+1,par+1,ans+str.charAt(i)+".", res);
+        
+         if(i+2<=str.length() && isValid(str.substring(i,i+2))==true)
+         {
+              helper(str,i+2,par+1,ans+str.substring(i,i+2)+".",res);
+         }
+        
+        if(i+3<=str.length() && isValid(str.substring(i,i+3))==true)
+        {
+              helper(str,i+3,par+1,ans+str.substring(i,i+3)+".",res);
+        }
+             
+    }
     
- }
+    boolean isValid(String str)
+    {
+        if(str.charAt(0)=='0') return false;
+        
+        int val = Integer.parseInt(str);
+        
+        return val<=255;
+    }
 }
