dca9dc0c7260b5decfefda29afec5308a77d3ced
Vishnudewangan <82579731+Vishnudewangan@users.noreply.github.com>
2023-03-12 09:40:50 +0530

    Time: 1 ms (100.00%), Space: 44.1 MB (73.55%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -9,74 +9,42 @@
  * }
  */
 class Solution {
-    public ListNode mergeKLists(ListNode[] lists) {
-        
-        if(lists==null || lists.length==0) return null;
-        
-        
-        if(lists.length==1) return lists[0];
-        
-        if(lists.length==2)
-        {
-            return mergeTwoList(lists[0],lists[1]);
-        }
-        
-       return mergeHelp(lists,0,lists.length-1);
-    }
-    
-   ListNode mergeTwoList(ListNode list1 , ListNode list2)
-    {
-        if(list1==null && list2==null) return null;
-        if(list1==null) return list2;
-        if(list2==null) return list1;
-        
+     public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null || l2 == null)
+            return l1 != null ? l1 : l2;
+
         ListNode dummy = new ListNode(-1);
-        
-        ListNode dt = dummy;
-        
-        ListNode curr1=list1;
-        ListNode curr2 = list2;
-        
-        while(curr1!=null && curr2!=null)
-        {
-          if(curr1.val < curr2.val )
-          {
-              dt.next= curr1;
-              curr1 = curr1.next;
-          }
-          else
-          {
-              dt.next = curr2;
-              curr2 = curr2.next;
-          }
-            
-        dt = dt.next;
+        ListNode prev = dummy;
+
+        ListNode c1 = l1;
+        ListNode c2 = l2;
+
+        while (c1 != null && c2 != null) {
+            if (c1.val <= c2.val) {
+                prev.next = c1;
+                c1 = c1.next;
+            } else {
+                prev.next = c2;
+                c2 = c2.next;
+            }
+            prev = prev.next;
         }
-        
-        if(curr1!=null)
-        {
-            dt.next= curr1;
-        }
-        if(curr2!=null)
-        {
-            dt.next = curr2;
-        }
-        
+
+        prev.next = (c1 != null ? c1 : c2);
+
         return dummy.next;
     }
-    
-    ListNode mergeHelp(ListNode[] lists, int start, int end)
-    {
-        
-        if(start== end)
-        {
-            return lists[start];
-        }
-         int mid = (start+end)/2;
-        
-        ListNode left = mergeHelp(lists,start,mid);
-        ListNode right = mergeHelp(lists,mid+1,end);
-        
-        return mergeTwoList(left,right);
+
+    public static ListNode mergeKLists(ListNode[] lists, int si, int ei) {
+        if (si == ei)
+            return lists[si];
+        int mid = (si + ei) / 2;
+        return mergeTwoLists(mergeKLists(lists, si, mid), mergeKLists(lists, mid + 1, ei));
     }
+    
+        public static ListNode mergeKLists(ListNode[] lists) {
+            if(lists.length ==0) return null;
+            
+           return mergeKLists(lists,0, lists.length-1);
+        }
 }
