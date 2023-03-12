e9d1464116c111337a6a4f261f1ceb5ec7551b76
Vishnudewangan <82579731+Vishnudewangan@users.noreply.github.com>
2023-03-12 10:03:54 +0530

    Time: 1 ms (100.00%), Space: 44.1 MB (64.15%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -9,42 +9,51 @@
  * }
  */
 class Solution {
-     public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l1 == null || l2 == null)
-            return l1 != null ? l1 : l2;
-
-        ListNode dummy = new ListNode(-1);
+    public ListNode mergeKLists(ListNode[] lists) {
+        
+        if(lists.length==0) return null;
+        
+        return mergeKLists(lists,0,lists.length-1);
+    }
+    ListNode mergeKLists(ListNode[] lists , int si, int ei)
+    {
+        if(si==ei) return lists[si];
+         
+        int mid = (si+ei)/2;
+        
+        return mergeTwoLists(mergeKLists(lists,si,mid),mergeKLists(lists,mid+1,ei));
+    }
+    
+    ListNode mergeTwoLists(ListNode list1 , ListNode list2)
+    {
+        if(list1==null && list2==null) return null;
+        
+        if(list1==null) return list2;
+        if(list2==null) return list1;
+        
+        ListNode dummy = new ListNode(0);
         ListNode prev = dummy;
-
-        ListNode c1 = l1;
-        ListNode c2 = l2;
-
-        while (c1 != null && c2 != null) {
-            if (c1.val <= c2.val) {
-                prev.next = c1;
+        
+        ListNode c1 = list1;
+        ListNode c2 = list2;
+        
+        while(c1!=null && c2!=null)
+        {
+            if(c1.val <= c2.val)
+            {
+                prev.next =c1;
                 c1 = c1.next;
-            } else {
+            }
+            else
+            {
                 prev.next = c2;
                 c2 = c2.next;
             }
             prev = prev.next;
         }
-
-        prev.next = (c1 != null ? c1 : c2);
-
+        
+        prev.next = c1!=null ? c1 : c2;
+        
         return dummy.next;
     }
-
-    public static ListNode mergeKLists(ListNode[] lists, int si, int ei) {
-        if (si == ei)
-            return lists[si];
-        int mid = (si + ei) / 2;
-        return mergeTwoLists(mergeKLists(lists, si, mid), mergeKLists(lists, mid + 1, ei));
-    }
-    
-        public static ListNode mergeKLists(ListNode[] lists) {
-            if(lists.length ==0) return null;
-            
-           return mergeKLists(lists,0, lists.length-1);
-        }
 }
