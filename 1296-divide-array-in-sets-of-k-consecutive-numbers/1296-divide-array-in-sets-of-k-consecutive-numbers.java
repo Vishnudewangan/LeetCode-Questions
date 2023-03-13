12923547c7caa9806958aa56cb01fe50ab68e711
Vishnudewangan <82579731+Vishnudewangan@users.noreply.github.com>
2023-03-13 22:11:52 +0530

    Time: 488 ms (17.87%), Space: 53.8 MB (54.55%) - LeetHub

---
+++ 1296-divide-array-in-sets-of-k-consecutive-numbers/1296-divide-array-in-sets-of-k-consecutive-numbers.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPossibleDivide(int[] nums, int k) {
+        PriorityQueue<Integer> pq =new PriorityQueue<>();
+        
+        for(int val : nums)pq.add(val);
+        
+        while(!pq.isEmpty())
+        {
+            int curr = pq.remove();
+            
+            for(int i=1;i<k;i++)
+            {
+               if(!pq.remove(curr+i)) return false;
+            }
+        }
+        return true;
+    }
+}
