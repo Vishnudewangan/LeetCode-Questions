0cac331cc3590690e5c2924eadc0fca0b09ad7d8
Vishnudewangan <82579731+Vishnudewangan@users.noreply.github.com>
2023-07-12 20:24:57 +0530

    Time: 9 ms (36.55%), Space: 44 MB (25.89%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,54 +1,57 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         
-        int i=-1;
-        int j=-1;
+         int i=-1, j=-1, olen = 0;
         
-        HashMap<Character,Integer> map =new HashMap<>();
+        HashMap<Character,Integer> map = new HashMap<>();// char v/s frequency
         
-        int maxLen = 0;
-        
-        while(i<s.length()-1)
-        {
-            while(i<s.length()-1)
-            {
-                i++;
-                
-                char ch = s.charAt(i);
-                
-               map.put(ch,map.getOrDefault(ch,0)+1);
-                
-                if(map.get(ch)==1)
-                {
-                   int len =i-j;
+         while(i< s.length()-1)
+         {
+              while(i<s.length()-1)
+              {
+                  i++;
+                  
+                  char ch = s.charAt(i);
+                  
+                  map.put(ch,map.getOrDefault(ch,0)+1);
+                  
+                  if(map.get(ch)>=2)
+                  {
+                      break;
+                  }
+                  else
+                  {
+                      int len = i-j;
+                      
+                      olen = Math.max(len , olen);
+                      
+                      
+                  }
+                  
+              }
+             
+             while(j<i)
+             {
+                 j++;
+                 
+                 char ch = s.charAt(j);
+                 
+                 if(map.get(ch)==1)
+                 {
+                     map.remove(ch);
                     
-                    maxLen = Math.max(len,maxLen);
-                }
-                else if(map.get(ch)==2)
-                {
-                    break;
-                }
-            }
-            
-            while(j<i)
-            {
-                j++;
-                char ch = s.charAt(j);
-                
-                int nf = map.get(ch);
-                
-                if(nf==1)
-                {
-                    map.remove(ch);
-                }
-                else if(nf==2)
-                {
-                    map.put(ch,1);
-                    break;
-                }
-            }
-        }
+                     
+                 }
+                 else
+                 {
+                     map.put(ch,map.get(ch)-1);
+                      break;
+                 }
+                 
+                 
+             }
+         }
         
-        return maxLen;
+        return olen;
     }
 }
