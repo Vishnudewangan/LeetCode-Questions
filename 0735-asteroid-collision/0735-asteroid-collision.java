83db3ac3d8a6700bcb2ba55b0dda97aeddaeb8a3
Vishnudewangan <82579731+Vishnudewangan@users.noreply.github.com>
2023-07-21 09:46:43 +0530

    Time: 12 ms (56.79%), Space: 44.2 MB (40.38%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,38 +1,38 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        Stack<Integer> st =new Stack<>();
+        Stack<Integer> st = new Stack<>();
         
         for(int val : asteroids)
         {
-            if(val > 0 ) 
+            if( val > 0 ) 
             {
-                 st.push(val);
+                st.push( val );
             }
             else
             {
-                while(st.size()>0 && st.peek()> 0 && st.peek() < -val ) st.pop();
+                while(st.size() > 0 && st.peek() > 0 && st.peek() < - val ) st.pop();
                 
-                if(st.size()> 0 && st.peek()==-val ) st.pop();
-                else if(st.size() > 0 && st.peek() > -val) {
-                    // we don't have to do anything   
+                if(st.size() > 0 && st.peek() == - val) st.pop();
+                else if(st.size() > 0 && st.peek() > - val) {
+                    
                 }
-                else{
-                    // val -> -ve
+                else // val is negative
+                {
                     st.push(val);
                 }
-                
             }
         }
         
         int[] ans = new int[st.size()];
         
-        int idx = ans.length-1;
+        int idx = st.size() - 1;
         
-        while(idx>=0)
+        while(idx >=0)
         {
             ans[idx--] = st.pop();
         }
         
         return ans;
+        
     }
 }
