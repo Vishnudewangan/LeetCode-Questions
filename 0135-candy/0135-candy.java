32037bc83f804eaa4ed0eccd852d317a674f44b3
Vishnudewangan <82579731+Vishnudewangan@users.noreply.github.com>
2023-07-21 22:51:23 +0530

    Time: 3 ms (74.00%), Space: 43.6 MB (91.77%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int candy(int[] ratings) {
+        
+         int[] left = new int[ratings.length];
+         int[] right = new int[ratings.length];
+        
+        Arrays.fill(left,1);
+        Arrays.fill(right,1);
+        
+        
+        for(int i=1;i<ratings.length;i++)
+        {
+            if(ratings[i-1] < ratings[i])
+            {
+                left[i] = left[i-1]+1;
+            }
+        }
+                                      
+        for(int i=ratings.length-2;i>=0;i--){
+            
+            if(ratings[i]> ratings[i+1])
+            {
+                right[i] = right[i+1]+1;
+            }
+        }
+                                    
+                                      
+        int reqCand = 0;
+                                      
+        for(int i=0;i<left.length;i++){
+            
+            reqCand +=Math.max(left[i], right[i]);
+        }
+                                      
+        return reqCand;
+                                      
+    }
+}
