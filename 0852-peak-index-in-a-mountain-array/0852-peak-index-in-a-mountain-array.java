226950c94322bdb88c5b0c0c2f333f17f9f46595
Vishnudewangan <82579731+Vishnudewangan@users.noreply.github.com>
2023-07-25 23:13:46 +0530

    Time: 0 ms (100.00%), Space: 55.3 MB (55.22%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -1,7 +1,25 @@
 class Solution {
      public int peakIndexInMountainArray(int[] A) {
-        for (int i = 1; i + 1 < A.length; ++i) if (A[i] > A[i + 1]) return i;
-        // for (int i = A.length - 1; i > 0; --i) if (A[i] > A[i - 1]) return i;
-        return 0;
+       
+         int left =0 , right = A.length-1;
+         
+         while(left < right )
+         {
+             int mid = left + (right - left)/2;
+             
+             
+             if(A[mid] < A[mid+1]) 
+             {
+                 left = mid + 1;
+             }
+             else 
+             {
+                  right = mid;
+             }
+             
+             
+         }
+         
+         return left;
     }
 }
