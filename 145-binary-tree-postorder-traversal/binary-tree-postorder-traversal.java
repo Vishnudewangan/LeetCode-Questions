1fa4df2a090b202e9b2e52e106116be7e33fbbf5
Vishnudewangan <82579731+Vishnudewangan@users.noreply.github.com>
2024-08-25 18:08:31 +0530

    Time: 1 ms (9.27%) | Memory: 41.9 MB (8.18%) - LeetSync

---
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -0,0 +1,68 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    static class Pair
+    {
+        int state;
+        TreeNode node;
+        Pair(int state , TreeNode node)
+        {
+            this.state = state;
+            this.node = node;
+        }
+    }
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> pre =new ArrayList<>();
+        List<Integer> in = new ArrayList<>();
+        List<Integer> post = new ArrayList<>();
+        
+        if(root == null) return post;
+        Stack<Pair> st =new Stack<>();
+
+        st.push(new Pair(0,root));
+       
+       while(st.size() > 0){
+        if(st.peek().state == 0)
+        {
+            st.peek().state++;
+            
+            pre.add(st.peek().node.val);
+
+            if(st.peek().node.left!=null)
+            {
+                st.push(new Pair(0,st.peek().node.left));
+            }
+
+        }
+        else if(st.peek().state == 1)
+        {
+            st.peek().state++;
+            in.add(st.peek().node.val);
+            if(st.peek().node.right!=null)
+            {
+                st.push(new Pair(0,st.peek().node.right));
+            }
+        }
+        else if(st.peek().state == 2)
+        {
+            post.add(st.peek().node.val);
+            st.pop();
+        }
+      }
+
+      return post;
+    }
+}
