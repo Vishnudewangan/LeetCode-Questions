2f755219c2ff249f09fcf2f2b3842a7de22f4e6a
Vishnudewangan <82579731+Vishnudewangan@users.noreply.github.com>
2025-02-11 21:10:32 +0530

    Time: 9 ms (19.41%) | Memory: 42.6 MB (73.57%) - LeetSync

---
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String removeOccurrences(String s, String part) {
+        return solve(new StringBuilder(s),part).toString();
+    }
+    private StringBuilder solve(StringBuilder sb, String part)
+    {
+        if(sb.length()==0) return sb;
+
+        int idx=  sb.indexOf(part);
+
+        if(idx==-1) return sb;
+
+        sb.delete(idx,idx+part.length());
+        System.out.println(sb);
+
+        StringBuilder ans = solve(sb,part);
+        return ans;
+        
+    }
+}
