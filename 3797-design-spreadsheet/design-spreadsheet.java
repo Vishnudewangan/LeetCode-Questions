bd77f68ea9d6562b79cac0448fbcebe02b9ea0ae
Vishnudewangan <82579731+Vishnudewangan@users.noreply.github.com>
2025-09-19 22:42:58 +0530

    Time: 123 ms (46.83%) | Memory: 56 MB (57.75%) - LeetSync

---
+++ 3797-design-spreadsheet/design-spreadsheet.java
@@ -0,0 +1,36 @@
+class Spreadsheet {
+    
+   int[][] spreadSheet;
+    public Spreadsheet(int rows) {
+        spreadSheet =new int[rows][26];
+    }
+    
+    public void setCell(String cell, int value) {
+        int col = (int)(cell.charAt(0)-'A');
+        int row = Integer.parseInt(cell.substring(1))-1;
+        spreadSheet[row][col] = value;
+    }
+    
+    public void resetCell(String cell) {
+          int col = (int)(cell.charAt(0)-'A');
+        int row = Integer.parseInt(cell.substring(1))-1;
+        spreadSheet[row][col] = 0;
+    }
+    
+    public int getValue(String formula) {
+        String[] parts = formula.substring(1).split("\\+");
+        //System.out.println(parts[0]+" "+parts[1]);
+
+        int val1 = !Character.isDigit(parts[0].charAt(0)) ? spreadSheet[Integer.parseInt(parts[0].substring(1))-1][(int)(parts[0].charAt(0)-'A')] : Integer.parseInt(parts[0]);
+        int val2 = !Character.isDigit(parts[1].charAt(0)) ? spreadSheet[Integer.parseInt(parts[1].substring(1))-1][(int)(parts[1].charAt(0)-'A')] : Integer.parseInt(parts[1]);
+        return val1 + val2;
+    }
+}
+
+/**
+ * Your Spreadsheet object will be instantiated and called as such:
+ * Spreadsheet obj = new Spreadsheet(rows);
+ * obj.setCell(cell,value);
+ * obj.resetCell(cell);
+ * int param_3 = obj.getValue(formula);
+ */
