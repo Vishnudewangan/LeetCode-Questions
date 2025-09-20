23f422ed3ad219f7e6f4f82f3f99a27d2a3944a0
Vishnudewangan <82579731+Vishnudewangan@users.noreply.github.com>
2025-09-20 11:37:25 +0530

    Time: 119 ms (63.86%) | Memory: 55.8 MB (87.37%) - LeetSync

--- 3797-design-spreadsheet/design-spreadsheet.java
+++ 3797-design-spreadsheet/design-spreadsheet.java
@@ -1,28 +1,32 @@
 class Spreadsheet {
     
-   int[][] spreadSheet;
+  // int[][] spreadSheet;
+    HashMap<String,Integer> spreadSheet;
     public Spreadsheet(int rows) {
-        spreadSheet =new int[rows][26];
+        // spreadSheet =new int[rows][26];
+        spreadSheet =new HashMap<>();
     }
     
     public void setCell(String cell, int value) {
-        int col = (int)(cell.charAt(0)-'A');
-        int row = Integer.parseInt(cell.substring(1))-1;
-        spreadSheet[row][col] = value;
+        // int col = (int)(cell.charAt(0)-'A');
+        // int row = Integer.parseInt(cell.substring(1))-1;
+        // spreadSheet[row][col] = value;
+        spreadSheet.put(cell,value);
     }
     
     public void resetCell(String cell) {
-          int col = (int)(cell.charAt(0)-'A');
-        int row = Integer.parseInt(cell.substring(1))-1;
-        spreadSheet[row][col] = 0;
+        //   int col = (int)(cell.charAt(0)-'A');
+        // int row = Integer.parseInt(cell.substring(1))-1;
+        // spreadSheet[row][col] = 0;
+        spreadSheet.remove(cell);
     }
     
     public int getValue(String formula) {
         String[] parts = formula.substring(1).split("\\+");
         //System.out.println(parts[0]+" "+parts[1]);
 
-        int val1 = !Character.isDigit(parts[0].charAt(0)) ? spreadSheet[Integer.parseInt(parts[0].substring(1))-1][(int)(parts[0].charAt(0)-'A')] : Integer.parseInt(parts[0]);
-        int val2 = !Character.isDigit(parts[1].charAt(0)) ? spreadSheet[Integer.parseInt(parts[1].substring(1))-1][(int)(parts[1].charAt(0)-'A')] : Integer.parseInt(parts[1]);
+        int val1 = !Character.isDigit(parts[0].charAt(0)) ?(spreadSheet.containsKey(parts[0]) ? spreadSheet.get(parts[0]) : 0): Integer.parseInt(parts[0]);
+        int val2 = !Character.isDigit(parts[1].charAt(0)) ? (spreadSheet.containsKey(parts[1]) ? spreadSheet.get(parts[1]) : 0) : Integer.parseInt(parts[1]);
         return val1 + val2;
     }
 }
