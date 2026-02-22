bfd0982c1f1f750515150e14f9f4cf9f92a16cc6
Vishnudewangan <82579731+Vishnudewangan@users.noreply.github.com>
2026-02-22 13:55:15 +0530

    Time: 18 ms (53.41%) | Memory: 82.8 MB (45.46%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -22,34 +22,25 @@ class Solution {
         return ans;
     }
     
-    private int check(int day,int bouquetsNeed, int adjacentFlowersNeed, int[] bloomDay){
+    private int check(int mid,int m, int k, int[] bloomDay){
+      
+      int adjacentFlowersTaken = 0;
+      int bouquetsPossible = 0;
 
+      for(int day = 0 ; day < bloomDay.length ; day++){
 
-       int adjacentFlowersTaken = bloomDay[0] <= day ? 1 : 0;
-       int bouquetsPossible = 0;// bouquesPossible on day'th day
-
-       for(int d=1 ; d < bloomDay.length ; d++){
-           if(adjacentFlowersTaken == adjacentFlowersNeed){
-               bouquetsPossible++;
-               adjacentFlowersTaken = bloomDay[d] <= day ? 1 : 0;
-               continue;
-           }
-
-           if(bloomDay[d] <= day && (adjacentFlowersTaken==0 || bloomDay[d-1] <= day)){
-                 adjacentFlowersTaken++;
-           }
-           else{
-            adjacentFlowersTaken = 0;
-           }
-       }
-
-        if(adjacentFlowersTaken == adjacentFlowersNeed){
-            bouquetsPossible++;
+        if(bloomDay[day] <= mid){
+            adjacentFlowersTaken++;
+        }
+        else{
+            bouquetsPossible +=(adjacentFlowersTaken / k);
             adjacentFlowersTaken = 0;
         }
 
-        if(bouquetsPossible >= bouquetsNeed) return 1;
-        else return 0;
+      }
+      bouquetsPossible += (adjacentFlowersTaken / k);
+
+      return bouquetsPossible >= m ? 1 : 0;
         
     }
 }
