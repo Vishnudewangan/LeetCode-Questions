22b2e875c118309760ebbd28ecc4d013bc1c162f
Vishnudewangan <82579731+Vishnudewangan@users.noreply.github.com>
2026-03-01 14:23:54 +0530

    Time: 44 ms (9.70%) | Memory: 45.5 MB (8.91%) - LeetSync

---
+++ 441-arranging-coins/arranging-coins.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        // search space will be 1 to n 
+
+        int lo = 1;
+        int hi = n-1;
+        int ans = 1;// min possible ans with atleast 1 coin
+        while(lo <= hi){
+          int mid = lo + (hi - lo)/2;
+
+          if(check(mid,n) == 1){
+            ans = mid;
+            lo = mid + 1;
+          }
+          else{
+            hi = mid -1;
+          }
+        }
+
+        return ans;
+    }
+    private int check(int row, int totalCoins){
+        long coinsRequired = ((long)row * ((long)row + 1))/2;
+
+        System.out.println("row : "+ row + " coinsRequired : " + coinsRequired);
+
+        return coinsRequired <= totalCoins ? 1 : 0 ;
+    }
+}
