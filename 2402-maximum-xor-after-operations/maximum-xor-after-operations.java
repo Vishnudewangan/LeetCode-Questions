d072e63881457dfc5989971840a003b98b2b3fdc
Vishnudewangan <82579731+Vishnudewangan@users.noreply.github.com>
2026-03-03 16:49:28 +0530

    Time: 1 ms (100.00%) | Memory: 59.4 MB (93.48%) - LeetSync

--- 2402-maximum-xor-after-operations/maximum-xor-after-operations.java
+++ 2402-maximum-xor-after-operations/maximum-xor-after-operations.java
@@ -3,15 +3,7 @@ class Solution {
        
        int ans = 0;
 
-       for(int i = 31; i >= 0; i--)
-        {   int mask = (1 << i);
-            int cnt = 0;
-            for(int j = 0 ; j < nums.length; j++){
-                
-                if((nums[j] & mask) >= 1) cnt++;
-            }
-            if(cnt > 0 ) ans ^= mask;
-       }
+       for(int val : nums) ans |= val;
 
        return ans;
     }
