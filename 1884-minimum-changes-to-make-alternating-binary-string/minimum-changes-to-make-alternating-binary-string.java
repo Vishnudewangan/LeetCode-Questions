93400fc6335e154bef80bfa6fc6e73916eca44ff
Vishnudewangan <82579731+Vishnudewangan@users.noreply.github.com>
2026-03-05 23:44:55 +0530

    Time: 6 ms (33.91%) | Memory: 43.6 MB (91.05%) - LeetSync

--- 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.java
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.java
@@ -1,44 +1,31 @@
 class Solution {
     public int minOperations(String s) {
 
-        StringBuilder startWithZero = new StringBuilder();
-        StringBuilder startWithOne = new StringBuilder();
-
         boolean zero = true;
         boolean one = true;
+       int changeWithZero = 0;
+       int changeWithOne = 0;
 
         for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
              if(zero){
-                startWithZero.append('0');
+               if(ch != '0')changeWithZero++;
              }
              else{
-                startWithZero.append('1');
+                if(ch !='1')changeWithZero++;
              }
 
              zero = !zero;
             
             if(one){
-                startWithOne.append('1');
+                if(ch != '1')changeWithOne++;
             }
             else{
-                startWithOne.append('0');
+                if(ch != '0')changeWithOne++;
             }
 
             one = !one;
 
-        }
-
-        int changeWithZero = 0;
-        int changeWithOne = 0;
-
-
-        for(int i = 0; i < s.length(); i++){
-            char ch = s.charAt(i);
-
-
-            if(ch != startWithZero.charAt(i))changeWithZero++;
-
-            if(ch != startWithOne.charAt(i)) changeWithOne++;
         }
 
         return Math.min(changeWithZero , changeWithOne);
